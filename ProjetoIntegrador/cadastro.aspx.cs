987c62f11f1be9e0285caca6ffa579ff70723148
jonhnny <jonathan99moura@hotmail.com>
2016-07-03 11:25:27 -0300

    Teste_new
    Hora do Show

---
+++ ProjetoIntegrador/cadastro.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace ProjetoIntegrador
+{
+    public partial class cadastro : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btncadastro_Click(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["teste7ConnectionString"].ConnectionString;
+
+            SqlConnection conexao = new SqlConnection(connStr);
+            conexao.Open();
+            SqlCommand insert = conexao.CreateCommand();
+
+            insert.CommandText = "INSERT INTO CadastroTeste(nome,email,senha) VALUES (@nome,@email,@senha)";
+            insert.Parameters.AddWithValue("@nome", nome.Text);
+            insert.Parameters.AddWithValue("@email", email.Text);
+            insert.Parameters.AddWithValue("@senha", senha.Text);
+
+            insert.ExecuteNonQuery();
+            Response.Redirect("~/cadastrando.aspx");
+        }
+    }
+}
---
+++ ProjetoIntegrador/login.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjetoIntegrador
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnentrar_Click(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["teste7ConnectionString"].ConnectionString;
+
+            SqlConnection conexao = new SqlConnection(connStr);
+            conexao.Open();
+            SqlCommand insert = conexao.CreateCommand();
+
+
+            insert.Parameters.AddWithValue("@email", email.Text);
+
+            String fdp = insert.CommandText = "select email,senha from CadastroTeste where email = 'email.Text' and senha = 'senha.Text'";
+            insert.ExecuteReader();
+
+             if (fdp == email.Text)
+             {
+                Response.Redirect("~/index.aspx");
+            }
+            
+            
+        
+        }
+    }
+}
