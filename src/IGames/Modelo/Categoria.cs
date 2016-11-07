c943417f8bdf53de131993ec124c3279339f4cc4
John-Luke <lucas_ennabledmu2@hotmail.com>
2016-11-07 16:36:06 -0300

    chore(): add Category model

---
+++ src/IGames/Modelo/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IGames.Modelo
+{
+    public class Categoria
+    {
+        public string Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Categoria(string Descricao, string Id)
+        {
+            this.Descricao = Descricao;
+            this.Id = Id;
+        }
+    }
+}
