65427619d0eea6aa9c2e05959144095780d361e2
John-Luke <lucas_ennabledmu2@hotmail.com>
2016-11-07 17:09:52 -0300

    chore(): add Comment model

---
+++ src/IGames/Modelo/Comentario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IGames.Modelo
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string UsuarioId { get; set; }
+
+        public Comentario(int Id, string Descricao, string UsuarioId)
+        {
+            this.Id = Id;
+            this.Descricao = Descricao;
+            this.UsuarioId = UsuarioId;
+        }
+    }
+}
