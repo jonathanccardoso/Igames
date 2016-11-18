744df7fc0d36d128a24852afab4b7dfd91501bb8
John-Luke <lucas_ennabledmu2@hotmail.com>
2016-11-18 10:01:08 -0300

    chore(): implement the method to add a game

--- src/IGames/Administrador/Online.aspx.cs
+++ src/IGames/Administrador/Online.aspx.cs
@@ -13,52 +13,40 @@ namespace IGames.Administrador
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            getCategories();
+            //getCategories();
         }
 
         protected void addGame_Click(object sender, EventArgs e)
         {
-
+            //<input type="file" webkitdirectory mozdirectory directory multiple></input>
+            uploadGame();
+            uploadImage();
+            DAL.DALGames jogo = new DAL.DALGames();
+            Modelo.Jogo jog = new Modelo.Jogo(jogo.SelectLast() + 1 ?? 1, "JogosDown/" + UploadGame.FileName, TextBox1.Text, "Images/" + UploadImage.FileName, TextBox2.Text, 0);
+            jogo.Insert(jog);
         }
 
         protected void getCategories()
         {
-            string aSQLConecStr;
-            aSQLConecStr = ConfigurationManager.ConnectionStrings["iGamesConnectionString"].ConnectionString;
-            SqlConnection aSQLCon = new SqlConnection(aSQLConecStr);
+            DAL.DALCategories cat = new DAL.DALCategories();
+            List<Modelo.Categoria> cats = cat.SelectAll();
+            Categorias.DataSource = cats;
+            foreach (Modelo.Categoria ca in cats) {
+                Categorias.DataTextField = ca.Descricao;
+                Categorias.DataValueField = ca.Id.ToString();
+            }
+            Categorias.DataBind();
+        }
 
-            try
-            {
-                using (aSQLCon)
-                {
-                    aSQLCon.Open();
-                    SqlCommand aSQL = new SqlCommand("SELECT * FROM Categoria", aSQLCon);
-                    SqlDataReader reader;
+        protected void uploadGame() {
+            
+            UploadGame.SaveAs("JogosDown/" + UploadGame.FileName);
+        }
 
-                    using (reader = aSQL.ExecuteReader())
-                    {
-                        if (reader.HasRows)
-                        {
-                            while (reader.Read())
-                            {
-                                Categorias.DataSource = reader;
-                                Categorias.DataTextField = "descricao";
-                                Categorias.DataValueField = "id";
-                                Categorias.DataBind();
-                            }
-                            reader.Close();
-                        }
-                    }
-                }
-            }
-            catch (SystemException)
-            {
-                throw;
-            }
-            finally
-            {
-                aSQLCon.Close();
-            }
+        //Add method to upload the game image
+        protected void uploadImage()
+        {
+            UploadImage.SaveAs("Images/" + UploadImage.FileName);
         }
     }
 }
