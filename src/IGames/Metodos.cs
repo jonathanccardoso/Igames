039119f1c97836af956f79b02d317f659b93217d
John-Luke <lucas_ennabledmu2@hotmail.com>
2016-12-09 11:59:18 -0300

    criando arquivo de metodos comuns

---
+++ src/IGames/Metodos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace IGames
+{
+    public class Metodos
+    {
+        public HttpSessionState Session { get; set; }
+
+        public static bool hasUser(string id)
+        {
+            return id == null ? true : false;
+        }
+
+        public static Modelo.Usuario getUser(string id)
+        {
+            return DAL.DALUsers.Select(id);
+        }
+
+        public static Modelo.Icone getIcon(int id)
+        {
+            return DAL.DALIcons.Select(id);
+        }
+
+        public void Sair()
+        {
+            //Versão 01
+            Session.Abandon();
+            //Versão 02
+            Session.Contents.RemoveAll();
+        }
+    }
+}
