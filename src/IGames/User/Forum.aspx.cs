93f89a89a87c7a5817780d9df61c519abe180bbe
John-Luke <lucas_ennabledmu2@hotmail.com>
2017-01-30 10:19:33 -0300

    ajeitando o forum (parte 01)

--- src/IGames/User/Forum.aspx.cs
+++ src/IGames/User/Forum.aspx.cs
@@ -11,44 +11,32 @@ namespace IGames.User
 {
     public partial class Forum : System.Web.UI.Page
     {
-        public DAL.DALForum dalforum { get; set; }
-
-        public List<Modelo.Forum> foruns { get; set; }
+        public List<Modelo.Forum> foruns { get { return DAL.DALForum.SelectAll(); } }
 
         public Modelo.Forum forum { get; set; }
 
-        public DAL.DALPosts dalposts { get; set; }
-
-        public List<Modelo.Postagem> postagens { get; set; }
+        public List<Modelo.Postagem> postagens { get { return DAL.DALPosts.SelectAll(); } set; }
 
         public Modelo.Postagem postagem { get; set; }
 
-        public DAL.DALUsers daluser { get; set; }
-
-        public List<Modelo.Usuario> users { get; set; }
+        public List<Modelo.Usuario> users { get { return DAL.DALUsers.SelectAll(); } set; }
 
         public Modelo.Usuario user { get; set; }
 
-        public DAL.DALIcons dalicon { get; set; }
-
-        public List<Modelo.Icone> icons { get; set; }
+        public List<Modelo.Icone> icons { get { return this.icons = DAL.DALIcons.SelectAll(); } set; }
 
         public Modelo.Icone icon { get; set; }
 
         protected void Page_Load(object sender, EventArgs e)
         {
             initPage();
-            getPosts();
-            getUsers();
-            getIcons();
-            getForuns();
         }
 
         protected void initPage()
         {
             if (Session["id"] != null)
             {
-                if (!Metodos.hasUser(Session["id"].ToString() ?? ""))
+                if (Metodos.hasUser(Session["id"].ToString() ?? ""))
                 {
                     this.user = Metodos.getUser(Session["id"].ToString());
                     this.icon = Metodos.getIcone(this.user.Icone_id);
@@ -64,65 +52,20 @@ namespace IGames.User
         {
             if (Request.QueryString["forum"] != null && Request.QueryString["postagem"] != null)
             {
-                this.dalforum = new DAL.DALForum();
                 this.forum = new Modelo.Forum(TextArea.Text, DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString(), DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute, user.id);
                 DAL.DALForum.Insert(forum);
             }
             //fazer comentario
             else if (Request.QueryString["forum"] != null)
             {
-                this.dalposts = new DAL.DALPosts();
                 this.postagem = new Modelo.Postagem(TextArea.Text, DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString(), DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute, user.id, Convert.ToInt32(Request.QueryString["forum"].ToString()));
                 DAL.DALPosts.Insert(postagem);
             }
             //responder comentario
             else if (Request.QueryString["postagem"] != null)
             {
-                this.dalposts = new DAL.DALPosts();
                 this.postagem = new Modelo.Postagem(TextArea.Text, DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString(), DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute, user.id, DAL.DALPosts.Select(Convert.ToInt32(Request.QueryString["postagem"].ToString())).Forum_id, Convert.ToInt32(Request.QueryString["postagem"].ToString()));
                 DAL.DALPosts.Insert(postagem);
-            }
-        }
-
-        protected void getForuns() {
-            this.foruns = DAL.DALForum.SelectAll();
-        }
-
-        protected void getPosts() {
-            this.postagens = DAL.DALPosts.SelectAll();
-        }
-
-        protected void hasUser()
-        {
-            if (!Page.IsPostBack)
-            {
-                if (Session["id"] == null)
-                {
-                    Response.Redirect("~/Public/Cadastro.aspx");
-                }
-            }
-        }
-
-        protected void getUser()
-        {
-            this.user = DAL.DALUsers.Select(Session["id"].ToString());
-        }
-
-        protected void getUsers()
-        {
-            this.users = DAL.DALUsers.SelectAll();
-        }
-
-        protected void getIcon()
-        {
-            this.icon = DAL.DALIcons.Select(this.user.Icone_id);
-        }
-
-        protected void getIcons()
-        {
-            if (!Page.IsPostBack)
-            {
-                this.icons = DAL.DALIcons.SelectAll();
             }
         }
 
